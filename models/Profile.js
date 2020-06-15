e571e5602862cd904709ac5fb0634c4aea56da10
Kay Kostadinov <KayKostadinov@users.noreply.github.com>
2020-06-15 20:39:41 +0300

    Added Profile model. Created GET, POST and UPDATE methods for user profiles

---
+++ models/Profile.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const ProfileSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    about: {
+        type: String
+    },
+    goals: {
+        type: [String]
+    },
+    interests: {
+        type: [String]
+    },
+    social: {
+        youtube: {
+            type: String
+        },
+        twitter: {
+            type: String
+        },
+        facebook: {
+            type: String
+        },
+        instagram: {
+            type: String
+        },
+        linkedin: {
+            type: String
+        }
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+module.exports = Profile = mongoose.model('profile', ProfileSchema);
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,12 +1,84 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+const { check, validationResult } = require('express-validator');
 
-// @route   GET api/profile
-// @desc    test
-// @access  public
+// @route   GET api/profile/me
+// @desc    get current profile
+// @access  private
+router.get('/me', auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.user.id }).populate('user', [name, avatar]);
 
-router.get('/', (req, res) => {
-    res.send('Profile route');
+        if (!profile) {
+            return res.status(400).json({ msg: 'Profile does not exist' });
+        }
+        res.json(profile);
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+})
+
+// @route   POST api/profile
+// @desc    create/ update a user profile
+// @access  private
+router.post('/', auth, async (req, res) => {
+    const {
+        about,
+        goals,
+        interests,
+    } = req.body;
+
+    const {
+        youtube,
+        twitter,
+        facebook,
+        instagram,
+        linkedin
+    } = req.body.social;
+
+    const profileFields = {};
+    profileFields.social = {};
+    profileFields.user = req.user.id;
+    if (about) profileFields.about = about;
+    if (youtube) profileFields.social.youtube = youtube;
+    if (twitter) profileFields.social.twitter = twitter;
+    if (facebook) profileFields.social.facebook = facebook;
+    if (instagram) profileFields.social.instagram = instagram;
+    if (linkedin) profileFields.social.linkedin = linkedin;
+    if (goals) {
+        profileFields.goals = goals.split(',').map(x => x.trim());
+    }
+    if (interests) {
+        profileFields.interests = interests.split(',').map(x => x.trim());
+    }
+
+    // save to DB
+    try {
+        let profile = await Profile.findOne({ user: req.user.id });
+
+        if (profile) { // Update
+            profile = await Profile.findOneAndUpdate(
+                { user: req.user.id },
+                { $set: profileFields },
+                { new: true }
+            );
+
+            return res.json(profile);
+        }
+        // Create
+        profile = new Profile(profileFields);
+
+        await profile.save();
+        res.json(profile);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
 })
 
 module.exports = router;
