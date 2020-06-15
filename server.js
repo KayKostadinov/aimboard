8cf32c4f94cb4faf6b132c3b18aaeefdc1319f27
Kay Kostadinov <KayKostadinov@users.noreply.github.com>
2020-06-15 10:33:11 +0300

    Added users, profile, posts, auth routes

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
-const connectDB = require('./config/db')
+const connectDB = require('./config/db');
 
-connectDB()
+connectDB();
 
-app.get('/', (req, res)=> {
-    res.send('Howdy')
+app.get('/', (req, res) => {
+    res.send('Howdy');
 })
 
-app.listen(PORT, ()=> {
+
+// Routes >>>
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/auth', require('./routes/api/auth'));
+
+
+
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
