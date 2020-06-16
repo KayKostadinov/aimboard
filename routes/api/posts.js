70c14d786238eb99e52ea324ea279438218f1e81
Kay Kostadinov <KayKostadinov@users.noreply.github.com>
2020-06-16 10:14:46 +0300

    Implemented comment functionality

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -147,4 +147,68 @@ router.put('/unlike/:id', auth, async (req, res) => {
     }
 })
 
+// @route   POST api/posts/comment/:id
+// @desc    comment on a post
+// @access  private
+
+router.post('/comment/:id', [auth, [
+    check('text', 'Text is required').not().isEmpty()
+]], async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        const post = await Post.findById(req.params.id);
+
+        const newComment = {
+            user: req.user.id,
+            text: req.body.text,
+            name: user.name,
+            avatar: user.avatar
+        };
+
+        post.comments.unshift(newComment);
+
+        await post.save();
+        res.json(post.comments);
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+});
+
+// @route   DELETE api/posts/comment/:post_id/:comment_id
+// @desc    delete a comment
+// @access  private
+
+router.delete('/comment/:post_id/:comment_id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        const comment = post.comments.find(x => x.id === req.params.comment_id);
+
+        if (!comment) {
+            return res.status(404).json({ msg: 'Comment not found' });
+        }
+
+        if (comment.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: 'Not authorized' });
+        }
+
+        const removeIndex = post.comments
+            .map(x => x.user.toString())
+            .indexOf(req.user.id);
+        post.comments.splice(removeIndex, 1);
+        await post.save();
+
+        res.json(post.comments);
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+})
 module.exports = router;
