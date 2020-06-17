521bfa51f4a7c85903efc899b5db73017dc0f96f
Kay Kostadinov <KayKostadinov@users.noreply.github.com>
2020-06-17 19:06:42 +0300

    Creating sign up/in forms

--- client/src/components/auth/Register.component.jsx
+++ client/src/components/auth/Register.component.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
 const Register = () => {
     return (
-        <div>
-            register
+        <div className="container">
+            <div className="form-container">
+                <h2>Sign Up</h2>
+                <form className="form" action="">
+                    <div className="form-group">
+                        <label htmlFor="name">Name</label>
+                        <input type="text" placeholder='name' name='name' required />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" placeholder='e-mail address' name='email' required />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" placeholder='password' name='password' required />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="password2">Confirm Password</label>
+                        <input type="password" placeholder='confirm password' name='password2' required />
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
