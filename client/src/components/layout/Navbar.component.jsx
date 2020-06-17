72cea6cd0a453a369039135cbbdab713d43b107c
Kay Kostadinov <KayKostadinov@users.noreply.github.com>
2020-06-17 13:03:51 +0300

    React cleanup and SCSS setup

---
+++ client/src/components/layout/Navbar.component.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className='navbar'>
+            <div className="logo">
+                <h1>
+                    <a href="/">
+                        <i className='fas fa-crosshairs' /> AimBoard
+                    </a>
+                </h1>
+            </div>
+            <div className="navigation">
+
+            </div>
+            <div className="profile">Profile</div>
+        </div>
+    )
+}
+
+export default Navbar;
